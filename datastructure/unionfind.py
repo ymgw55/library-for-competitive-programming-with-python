931a217a804a34744cb2818e46d0744225665c25
ymgw55 <ymgw55@gmail.com>
2020-04-14 18:15:41 +0900

    fix union find

--- datastructure/unionfind.py
+++ datastructure/unionfind.py
@@ -1,30 +1,33 @@
-class UnionFind:
+class UnionFind():
 
     def __init__(self, N):
-        self.follow = [-1]*(N+1)
-        self.num_follower = [1]*(N+1)
+        self.__follow = [-1]*(N+1)
+        self.__num_follower = [1]*(N+1)
 
-    def root_index_of(self, a):
+    def root(self, a):
         r = a
-        while self.follow[r] > -1:
-            r = self.follow[r]
+        while self.__follow[r] > -1:
+            r = self.__follow[r]
         return r
     
-    def connet(self, a, b):
-        ra = self.root_index_of(a)
-        rb = self.root_index_of(b)
+    def merge(self, a, b):
+        ra = self.root(a)
+        rb = self.root(b)
     
         if ra == rb:
             return
         
-        if self.num_follower[ra] < self.num_follower[rb]:
-            self.follow[ra] = rb
-            self.follow[a] = rb
-            self.num_follower[rb] += self.num_follower[ra]
+        if self.__num_follower[ra] < self.__num_follower[rb]:
+            self.__follow[ra] = rb
+            self.__follow[a] = rb
+            self.__num_follower[rb] += self.__num_follower[ra]
         else:
-            self.follow[rb] = ra
-            self.follow[b] = ra
-            self.num_follower[ra] += self.num_follower[rb]
+            self.__follow[rb] = ra
+            self.__follow[b] = ra
+            self.__num_follower[ra] += self.__num_follower[rb]
     
-    def conneted(self, a, b):
-        return self.root_index_of(a) == self.root_index_of(b)
+    def issame(self, a, b):
+        return self.root(a) == self.root(b)
+
+    def size(self, a):
+        return self.__num_follower(self.root(a))
---
+++ tests/test_unionfind.py
@@ -0,0 +1,61 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from datastructure.unionfind import UnionFind
+import unittest
+
+class TestUnionFind(unittest.TestCase):
+    """
+    test class of UnionFind
+    """
+
+    def test_sample1(self):
+        """
+        test method for UnionFind
+
+        at first, N is given, and make a disjoint set whose elements are 0, 1,..., N-1, 
+        and then Q and Query are given.
+        Q is the size of Query and Query consits of (p1, x1, y1), (p2, x2, y2)...(pQ, xQ, yQ).
+        p is the marker of the type of query and x, y are elements.
+
+
+        the type of query:
+        if p = 0, unite x and y.
+        if p = 1, check whether x, y are united and return True or False.
+
+
+        "expected" is the expected answer list, 
+        and "output" is the output list whose elements are added when p = 1.
+
+        """
+
+        N = 8
+        U = UnionFind(N)
+
+        Q = 9
+        Query = [
+            (0, 1, 2),
+            (0, 3, 2),
+            (1, 1, 3),
+            (1, 1, 4),
+            (0, 2, 4),
+            (1, 4, 1),
+            (0, 4, 2),
+            (0, 0, 0),
+            (1, 0, 0)
+        ]
+        expected = [True, False, True, True]
+        output = []
+
+        for i in range(Q):
+            p, x, y = Query[i]
+            if p:
+                output.append((U.issame(x, y)))
+            else:
+                U.merge(x, y)
+        self.assertEqual(expected, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
